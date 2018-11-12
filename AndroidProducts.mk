fe49e6be83dbb4796d54177ba4610aaa6d7bcb05
oshmoun <oshmoun100@googlemail.com>
2018-11-12 02:47:08 +0100

    sony: apollo: Omnify
    Change-Id: Iced35d7f6162333fd65f32cf4398b6d5655dce66

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,5 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_h8314.mk \
-                     $(LOCAL_DIR)/aosp_h8324.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_apollo.mk
